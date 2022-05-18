fd6e468ed13197466883dd246a7229864a490837
Ben Taylor <63203762+BenHiramTaylor@users.noreply.github.com>
2022-05-18 20:54:16 +0100

    refactored

--- Create.py
+++ Create.py
@@ -1,4 +1,3 @@
-import json
 import requests
 import os
 import subprocess
@@ -8,53 +7,52 @@ username = 'USERNAME HERE'
 api_key = 'API KEY HERE'
 
 
-def makerepo(reponame):
-    global username, api_key
-    req = requests.post('https://api.github.com/user/repos', data=json.dumps({'name': reponame, 'private': 'true'}),
-                        auth=(username, api_key))
-    if req.status_code == 200 or req.status_code == 201:
+def make_remote_repository(repository_name, username, api_key):
+    request = requests.post('https://api.github.com/user/repos', json={'name': repository_name, 'private': 'true'}, auth=(username, api_key))
+
+    if request.ok:
         print(f'Repo Creation Successful.')
     else:
-        print(f'Got Status Code: {req.status_code}, Please Investigate')
-        print(req.text)
-    localgit(reponame)
+        print(f'Got Status Code: {request.status_code}, Please Investigate')
+        print(request.text)
+
+    create_local_git_repository(repository_name, username)
 
 
-def localgit(reponame):
-    global username
+def create_local_git_repository(repository_name, username):
     if sys.platform == 'darwin':
         print('System is Mac')
-        homedir = os.path.expanduser('~').split('/')[-1]
-        if not os.path.exists(f'/Users/{homedir}/Code Projects'):
-            os.mkdir(f'/Users/{homedir}/Code Projects')
-        localrepo = f'/Users/{homedir}/Code Projects/{reponame}'
-        os.mkdir(localrepo)
+        home_directory = os.path.expanduser('~').split('/')[-1]
+        if not os.path.exists(f'/Users/{home_directory}/Code Projects'):
+            os.mkdir(f'/Users/{home_directory}/Code Projects')
+        local_repository_path = f'/Users/{home_directory}/Code Projects/{repository_name}'
+        os.mkdir(local_repository_path)
     elif sys.platform == 'linux':
         print('System is Linux')
-        homedir = os.path.expanduser('~').split('/')[-1]
-        if not os.path.exists(f'/home/{homedir}/Code Projects'):
-            os.mkdir(f'/home/{homedir}/Code Projects')
-        localrepo = f'/home/{homedir}/Code Projects/{reponame}'
-        os.mkdir(localrepo)
+        home_directory = os.path.expanduser('~').split('/')[-1]
+        if not os.path.exists(f'/home/{home_directory}/Code Projects'):
+            os.mkdir(f'/home/{home_directory}/Code Projects')
+        local_repository_path = f'/home/{home_directory}/Code Projects/{repository_name}'
+        os.mkdir(local_repository_path)
     elif sys.platform == 'win32':
         print('System is Windows')
-        homedir = os.path.expanduser('~').split('/')[-1]
-        if not os.path.exists(f'C:/Users/{homedir}/Code Projects'):
-            os.mkdir(f'C:/Users/{homedir}/Code Projects')
-        localrepo = f'C:/Users/{homedir}/Code Projects/{reponame}'
-        os.mkdir(localrepo)
+        home_directory = os.path.expanduser('~').split('/')[-1]
+        if not os.path.exists(f'C:/Users/{home_directory}/Code Projects'):
+            os.mkdir(f'C:/Users/{home_directory}/Code Projects')
+        local_repository_path = f'C:/Users/{home_directory}/Code Projects/{repository_name}'
+        os.mkdir(local_repository_path)
     else:
         raise Exception('Could not recognise local OS.')
 
-    subprocess.check_call(['git', 'init'], cwd=localrepo)
-    with open(f'{localrepo}/README.md', 'w') as f:
+    subprocess.check_call(['git', 'init'], cwd=local_repository_path)
+    with open(f'{local_repository_path}/README.md', 'w') as f:
         f.close()
-    subprocess.check_call(['git', 'add', 'README.md'], cwd=localrepo)
-    subprocess.check_call(['git', 'commit', '-m', 'Initial Commit'], cwd=localrepo)
-    subprocess.check_call(['git', 'remote', 'add', 'origin', f'https://github.com/{username}/{reponame}.git'],
-                          cwd=localrepo)
-    subprocess.check_call(['git', 'push', '-u', 'origin', 'master'], cwd=localrepo)
+    subprocess.check_call(['git', 'add', 'README.md'], cwd=local_repository_path)
+    subprocess.check_call(['git', 'commit', '-m', 'Initial Commit'], cwd=local_repository_path)
+    subprocess.check_call(['git', 'remote', 'add', 'origin', f'https://github.com/{username}/{repository_name}.git'],
+                          cwd=local_repository_path)
+    subprocess.check_call(['git', 'push', '-u', 'origin', 'master'], cwd=local_repository_path)
 
 
 if __name__ == '__main__':
-    makerepo(sys.argv[1])
+    make_remote_repository(sys.argv[1], username, api_key)
